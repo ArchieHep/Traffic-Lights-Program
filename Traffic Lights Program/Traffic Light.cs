535195e81eeb2397a1a926ac87db63e91e85e4e5
Archiehep <433823@edu.cwa.ac.uk>
2024-06-24 15:32:30 +0100

    Added Traffic Light

---
+++ Traffic Lights Program/Traffic Light.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traffic_Lights_Program
+{
+    internal class Traffic_Light
+    {
+        private Bulb _red, _amber, _green;
+
+    }
+}
